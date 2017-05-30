857292a2996d1a5437e33bf7f989822c85a3728f
anujashelke <anuja_shelke@outlook.com>
2017-05-30 16:36:11 +0530

    submitted

---
+++ app.js
@@ -0,0 +1,23 @@
+var Employee = [
+
+              "Anuja",
+               
+               25,
+ 
+               25000,
+
+               "Amravati",
+               
+               "Maharashrta",
+               
+                444604
+
+               ]
+
+for(var E in Employee)
+
+{
+
+document.write(Employee[E] + "<br \>")
+
+}
